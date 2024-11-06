bc58b9d83007d8dadbf77a32da3f6e4660347c26
Barbora Krejčová <krejcova.barbora96@gmail.com>
2024-11-06 14:24:05 +0100

    hotovoy ukol

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+const question = document.querySelector('.question');
+question.textContent = 'Uveďte jméno nejnovějšího prezidenta USA';
 
+const actualAnswer = prompt('Uveďte jméno nejnovějšího prezidenta USA', 'Trump/Harris').trim();
+
+const answerText = document.querySelector('.answer__text');
+answerText.innerHTML = `<p>${actualAnswer}</p>`;
+
+const answer = document.querySelector('.answer');
+
+if (actualAnswer === 'Trump') {
+    answer.classList.add('answer--correct');
+} else {
+    answer.classList.add('answer--wrong');
+}
+
+//Reseni z webu:
+
+/*
+const questionElm = document.querySelector('.question');
+const question =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+
+questionElm.textContent = question;
+
+const userAnswer = prompt(question);
+
+const answerTextElm = document.querySelector('.answer__text');
+answerTextElm.textContent = userAnswer;
+
+const answerElm = document.querySelector('.answer');
+
+if (userAnswer === 'Bell') {
+  answerElm.classList.add('answer--correct');
+} else {
+  answerElm.classList.add('answer--wrong');
+}
+  */
